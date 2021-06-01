0504a8abd97bac36bda96b8ab45822a74ebef18d
Henry James Cinco <henryjamescinco@gmail.com>
2021-06-01 20:22:31 +0800

    initial codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header.js';
+import Banner from './Components/Banner/Banner.js';
+import Middle from './Components/Middle/Middle.js';
+import Bottom1 from './Components/Bottom1/Bottom1.js';
+import Bottom2 from './Components/Bottom2/Bottom2.js';
+import Bottom3 from './Components/Bottom3/Bottom3.js';
+import Bottom4 from './Components/Bottom4/Bottom4.js';
+import Footer from './Components/Footer/Footer.js';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <Middle />
+      <Bottom1 />
+      <Bottom2 />
+      <Bottom3 />
+      <Bottom4 />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Bottom1/Bottom1.js
@@ -0,0 +1,39 @@
+import './Bottom1.scss';
+import Card from '../Card/Card.js';
+
+function Bottom1() {
+    return (
+    <section id="bottom1" class="bottom1">
+        <section class="wrapper">
+            <header class="bottom1__header">
+                <h2>Best Seller</h2>
+                <nav class="bottom1__nav">
+                    <ul>
+                        <li class="bottom1__menu bottom1__menu--active"><a href="#">All</a></li>
+                        <li class="bottom1__menu"><a href="#">Mac</a></li>
+                        <li class="bottom1__menu"><a href="#">iPhone</a></li>
+                        <li class="bottom1__menu"><a href="#">iPad</a></li>
+                        <li class="bottom1__menu"><a href="#">iPod</a></li>
+                        <li class="bottom1__menu"><a href="#">Accessories</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <article class="bottom1__products">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </article>
+            <a href="#" class="bottom1__button">Load More</a>
+        </section>
+    </section>
+
+    )
+}
+
+export default Bottom1;
