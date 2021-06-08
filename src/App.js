9ac49f4590e24396edd6feace2b3f0c90245cc50
Henry James Cinco <henryjamescinco@gmail.com>
2021-06-08 15:42:19 +0800

    index working

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Header from './Components/Header/Header.js';
 import Banner from './Components/Banner/Banner.js';
 import Middle from './Components/Middle/Middle.js';
 import Bottom1 from './Components/Bottom1/Bottom1.js';
-<<<<<<< HEAD
-=======
 import Bottom2 from './Components/Bottom2/Bottom2.js';
 import Bottom3 from './Components/Bottom3/Bottom3.js';
 import Bottom4 from './Components/Bottom4/Bottom4.js';
 import DisplayProduct from './Components/DisplayProduct/DisplayProduct.js';
 import Footer from './Components/Footer/Footer.js';
-
->>>>>>> hotfix/revert1
 
 function App() {
   return (
@@ -20,15 +16,11 @@ function App() {
       <Banner />
       <Middle />
       <Bottom1 />
-<<<<<<< HEAD
-=======
       <Bottom2 />
       <Bottom3 />
       <Bottom4 />
       <DisplayProduct />
-      <Footer />
-
->>>>>>> hotfix/revert1
+       <Footer />
     </div>
   );
 }
