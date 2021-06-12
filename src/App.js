8689a2412f124bc22d7bb581776fc00ada3a599b
Henry James Cinco <henryjamescinco@gmail.com>
2021-06-12 16:15:30 +0800

    for pull

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ import Bottom2 from './Components/Home/Bottom2/Bottom2.js';
 import Bottom3 from './Components/Home/Bottom3/Bottom3.js';
 import Bottom4 from './Components/Home/Bottom4/Bottom4.js';
 import Footer from './Components/Footer/Footer.js';
+import { Router } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <Middle />
-      <Bottom1 />
-      <Bottom2 />
-      <Bottom3 />
-      <Bottom4 />
-      <Footer />
-    </div>
+      <div className="App">
+        <Router  >
+          <Header />
+            <Route path="/" exact>
+              <Banner />
+              <Middle />
+              <Bottom1 />
+              <Bottom2 />
+              <Bottom3 />
+              <Bottom4 />
+              <Footer />
+            </Route>
+        </Router>
+      </div>
   );
 }
 
